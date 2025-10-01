071a982be619a7cc5a3781c812d3dcf2d8c7701f
MIFZALAQIL\mifza <mifzal.aqil@gmail.com>
2025-10-01 10:54:24 +0800

    serve static

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+   images: {
+    unoptimized: true, // 👈 disables Next.js image optimizer for static export
+  } 
 };
 
 export default nextConfig;
